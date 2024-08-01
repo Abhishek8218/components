e950dbe6ab5ac2a27f195c9304be7b46208d3f56
Abhishek Kumar <ak3037308@gmail.com>
2024-08-01 23:42:50 +0530

    updated the fly to user location feature

--- src/app/components/LocationPicker.tsx
+++ src/app/components/LocationPicker.tsx
@@ -1,11 +1,7 @@
-'use client';
-
-
-  
 import React, { useState, useEffect } from 'react';
-import { MapContainer, Marker, TileLayer, useMapEvents } from 'react-leaflet';
+import { MapContainer, Marker, TileLayer, useMapEvents, useMap } from 'react-leaflet';
 import { MaterialSymbol } from 'react-material-symbols';
-import 'leaflet/dist/leaflet.css'
+import 'leaflet/dist/leaflet.css';
 import L from 'leaflet';
 
 interface ZoomFeatures {
@@ -17,47 +13,80 @@ interface ZoomFeatures {
 
 interface LocationPickerProps {
   initialPosition: [number, number];
-  
   zoomFeatures: ZoomFeatures;
   markerIcon: React.ReactNode;
   children?: React.ReactNode;
-  userPosition?: [number, number];
+  fetchLocation: boolean;
+
+  userPosition: [number, number];
   userLocationIconUrl: string;
-  handleLocationSearch: Function;
 }
 
-
-
-
-
-const LocationPicker = ({ initialPosition, children,zoomFeatures,markerIcon,userPosition,userLocationIconUrl,handleLocationSearch}: LocationPickerProps) => {
+const LocationPicker = ({
+  initialPosition,
+  children,
+  zoomFeatures,
+  markerIcon,
+  userPosition,
+  fetchLocation
+ 
+}: LocationPickerProps) => {
   const [position, setPosition] = useState<[number, number]>(initialPosition);
 
 
-const { minZoom, zoom, maxZoom, zoomControl} = zoomFeatures;
+  const { minZoom, zoom, maxZoom, zoomControl } = zoomFeatures;
+
+  
 
 
-// Map events
+
+  const [hasFlownToUser, setHasFlownToUser] = useState<boolean>(false);
+
+
+//Component to fly to user location
+  const UserLocation = () => {
+    const map = useMap();
+    useEffect(() => {
+      if (fetchLocation && !hasFlownToUser) {
+        console.log('Flying to user position:', userPosition);
+        map.flyTo(userPosition, zoom);
+        setHasFlownToUser(true);
+      }
+    }, [fetchLocation, hasFlownToUser, userPosition, map, zoom]);
+
+    return null;
+  };
+
+
+
+
+
+
   const MapEvents = () => {
+    const map = useMap();
     useMapEvents({
-      moveend: (event) => {
-        const map = event.target;
+      moveend: () => {
         const center = map.getCenter();
         setPosition([center.lat, center.lng]);
       },
     });
+
+ 
+    //   map.setView(userPosition);
+      console.log('userPosition in map events:', userPosition);
+    
     return null;
   };
-    
-  useEffect(() => {
-    console.log('Current position:', position);
-  }, [position]);
+
+  
+
+
 
 
 
 
   // Create custom icon for user location
-const createCustomIcon = (color: string) => {
+  const createCustomIcon = (color: string) => {
     return new L.DivIcon({
       html: `<div style="background-color: ${color}; width: 24px; height: 24px; border-radius: 50%; border: 4px solid white;"></div>`,
       iconSize: [24, 24],
@@ -67,12 +96,11 @@ const createCustomIcon = (color: string) => {
 
   const userLocationIcon = createCustomIcon('#0e4ce9');
 
-  const handleRecenter = () => {
-    setPosition([userPosition?.[0] ?? 0, userPosition?.[1] ?? 0]);//ToDo: FIx this to recenter the map
-  }
-  
+    // const map = useMap();
+    // map.flyTo(userPosition, zoom);
+
+
   return (
-    
     <div className="relative h-screen w-screen">
       <div className="absolute text-red-500 z-[1000] w-full flex justify-center items-center mt-2">
         {children}
@@ -81,40 +109,34 @@ const createCustomIcon = (color: string) => {
         center={position}
         minZoom={minZoom}
         zoom={zoom}
-
         maxZoom={maxZoom}
         zoomControl={zoomControl}
         className="h-full w-full"
       >
         <TileLayer
           url="https://api.mapbox.com/styles/v1/upaharhousenepal/cl1tf387y004214mvfq2utlaw/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoidXBhaGFyaG91c2VuZXBhbCIsImEiOiJjbDAycmttNG4xNnE5M2Nwa3VvbmczdjgxIn0.8DwZzdG46rwqvzrYVcUI4g"
-        maxNativeZoom={22}
-        maxZoom={22}
-        
+          maxNativeZoom={22}
+          maxZoom={22}
         />
 
-{userPosition && (
-          <Marker position={userPosition} icon={userLocationIcon}/>
-        
-        )}
+        {userPosition && <Marker position={userPosition} icon={userLocationIcon} />}
         <MapEvents />
+        <UserLocation/>
         <div className="absolute top-1/2 left-1/2 w-8 h-10 transform -translate-x-1/2 -translate-y-full z-[999]">
-{markerIcon}
-        </div >
-
-        <div>
-        <MaterialSymbol  icon="my_location" size={32}  fill
-          grade={-25}
-        onClick={handleRecenter}
-          className='absolute bottom-10 right-10 z-[999]'
-    />
-
+          {markerIcon}
         </div>
-     
+
+        <MaterialSymbol
+          icon="my_location"
+          size={32}
+          fill
+          grade={-25}
+       onClick={() => {setHasFlownToUser(false)}}
+          className="absolute bottom-20 right-10 z-[999]"
+        />
       </MapContainer>
     </div>
   );
 };
-
 
 export default LocationPicker;
--- src/app/pick/page.tsx
+++ src/app/pick/page.tsx
@@ -1,74 +1,64 @@
-
 'use client';
 
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react';
 import dynamic from 'next/dynamic';
 import { MaterialSymbol } from 'react-material-symbols';
 
-
 const LocationPicker = dynamic(() => import('../components/LocationPicker'), {
-    ssr: false,
-  });
-
-
+  ssr: false,
+});
 
 const Picker = () => {
-const [userPosition, setUserPosition] = useState<[number, number]>([0,0])
-const [fetchLocation,setFetchLocation] = useState(false)
+  const [userPosition, setUserPosition] = useState<[number, number]>([0, 0]);
+  const [fetchLocation, setFetchLocation] = useState(false);
 
-
-
-    useEffect(() => {
-        if (navigator.geolocation) {
-          navigator.geolocation.getCurrentPosition(
-            (position) => {
-              setUserPosition([position.coords.latitude, position.coords.longitude]);
-            },
-            (error) => {
-              console.error("Error getting user location:", error);
-              setFetchLocation(false)
-            }
-          );
+  useEffect(() => {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          setUserPosition([position.coords.latitude, position.coords.longitude]);
+          setFetchLocation(true); // Recenter map after getting the position
+      
+          console.log('User position: in pick pagw', userPosition);
+        },
+        (error) => {
+          console.error('Error getting user location:', error);
+          setFetchLocation(false);
         }
-      },[ ]);
-
-
-    const initialPosition: [number, number] = [userPosition[0], userPosition[1]];   
-    const zoomFeatures = {
-       minZoom:  18,
-      zoom : 20,
-       maxZoom:  22,
-       zoomControl : false,
+      );
     }
-   const  markerIcon = (
-    <MaterialSymbol
-    icon="location_on"
-    size={32}
-    fill
-    grade={-25}
-    color="black"
-  />
-   );
+  }, []);
+ 
 
-   const handleLocationSearch = () => {
-    setFetchLocation(true); // Trigger location fetch
+  const initialPosition: [number, number] = [userPosition[0], userPosition[1]];
+  const zoomFeatures = {
+    minZoom: 18,
+    zoom: 20,
+    maxZoom: 22,
+    zoomControl: false,
   };
-  
-
+  const markerIcon = (
+    <MaterialSymbol
+      icon="location_on"
+      size={32}
+      fill
+      grade={-25}
+      color="black"
+    />
+  );
 
   return (
-    <div className='h-screen w-full'> 
-<LocationPicker  initialPosition={initialPosition}
+    <div className="h-screen w-full">
+      <LocationPicker
+        initialPosition={initialPosition}
         zoomFeatures={zoomFeatures}
         markerIcon={markerIcon}
         userPosition={userPosition}
-        userLocationIconUrl={"https://maps.google.com/mapfiles/ms/icons/blue-dot.png"}
-       handleLocationSearch={handleLocationSearch}
-        />
-
-        
+        fetchLocation={fetchLocation}
+        userLocationIconUrl="https://maps.google.com/mapfiles/ms/icons/blue-dot.png"
+      />
     </div>
-  )
-}
+  );
+};
 
-export default Picker
+export default Picker;
